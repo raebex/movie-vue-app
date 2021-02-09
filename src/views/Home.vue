aadd1e12210b8172b108f36a6aab5b85fefe20f5
rebecca <r.young1025@gmail.com>
2021-02-09 16:40:30 -0600

    wire up flows to list movies and add a new movie

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,78 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h2>Add a new movie:</h2>
+    Title: <input type="text" v-model="newTitle">
+    <br>
+    Year: <input type="text" v-model="newYear">
+    <br>
+    Plot: <input type="text" v-model="newPlot">
+    <br>
+    Director: <input type="text" v-model="newDirector">
+    <br>
+    <button v-on:click="addMovie">Add movie</button>
+    
+    <ul v-for="movie in movies" :key="movie.id">
+      <li>
+        <h2>{{ movie.title }}</h2>
+        <p>Year: {{ movie.year }}</p>
+        <p>Plot: {{ movie.plot }}</p>
+        <p>Director: {{ movie.director }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function() {
+    return {
+      movies: [],
+      newTitle: "",
+      newYear: "",
+      newPlot: "",
+      newDirector: ""
+    };
   },
+  created: function() {
+    this.listMovies();
+  },
+  methods: {
+    listMovies: function() {
+      axios
+        .get("/api/movies")
+        .then(response => {
+          this.movies = response.data;
+        })
+        .catch(error => {
+          console.log(error.response.data);
+        });
+    },
+    addMovie: function() {
+      var params = {
+        title: this.newTitle,
+        year: this.newYear,
+        plot: this.newPlot,
+        director: this.newDirector
+      };
+
+      axios
+        .post("/api/movies", params, {
+          headers: {
+            Authorization: "Bearer [YOUR_JWT_TOKEN]"
+          }
+        })
+        .then(response => {
+          this.movies.push(response.data);
+        })
+        .catch(error => {
+          console.log(error.response.data);
+        });
+    }
+  }
 };
 </script>
